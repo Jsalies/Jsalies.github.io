9426128e47b00ca76dbc9563bb549eddd4c76a7e
Jimmy <jimmy.salies@etu.emse.fr>
2016-12-31 21:18:55 +0100

    commit 10: ajustement Nom de site + barre de jeux

--- Website/JS/responsiveslides.js
+++ Website/JS/responsiveslides.js
@@ -14,12 +14,12 @@
     // Default settings
     var settings = $.extend({
       "auto": true,             // Boolean: Animate automatically, true or false
-      "speed": 500,             // Integer: Speed of the transition, in milliseconds
-      "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
+      "speed": 1000,             // Integer: Speed of the transition, in milliseconds
+      "timeout": 5000,          // Integer: Time between slide transitions, in milliseconds
       "pager": false,           // Boolean: Show pager, true or false
       "nav": false,             // Boolean: Show navigation, true or false
       "random": false,          // Boolean: Randomize the order of the slides, true or false
-      "pause": false,           // Boolean: Pause on hover, true or false
+      "pause": true,           // Boolean: Pause on hover, true or false
       "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
       "prevText": "Previous",   // String: Text for the "previous" button
       "nextText": "Next",       // String: Text for the "next" button
