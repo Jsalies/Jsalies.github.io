76cd63b1886c128e9048c9e8674eabfc04c1aa5f
Jimmy <jimmy.salies@etu.emse.fr>
2017-01-05 23:29:07 +0100

    commit 17: remplissage de plusieurs pages + ajustement barres de menus.

---
+++ Website/JS/contact.js
@@ -0,0 +1,9 @@
+var element1= document.getElementById('nom');
+var element2= document.getElementById('tete2');
+
+function crier() {
+    alert('heyy');
+};
+
+
+element1.addEventListener('click', crier());
