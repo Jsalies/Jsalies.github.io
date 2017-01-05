0dcecb658e9ee4c8401f82548f21ec9972c46b86
Jimmy <jimmy.salies@etu.emse.fr>
2017-01-06 00:10:09 +0100

    commit 18: ajout js pour contact (+ incomprehension sur un bug)

--- Website/JS/contact.js
+++ Website/JS/contact.js
@@ -1,9 +1,37 @@
-var element1= document.getElementById('nom');
+var element1= document.getElementById('tete1');
 var element2= document.getElementById('tete2');
 
-function crier() {
-    alert('heyy');
+function premier() {
+    $('#info').empty();
+    $('#1').empty();
+    $('#2').empty();
+    $('#3').empty();
+    $('#4').empty();
+    $('#5').empty();
+    $('#6').empty();
+    $('#1').append('Age : 21ans');
+    $('#2').append('Type de jeux : Mmorpg, Mmoba');
+    $('#3').append('Jeux du moment: lol, minecraft');
+    $('#4').append('Pseudo(par jeux) : Deathxchaos(lol), IggDrazil(minecraft)');
+    $('#5').append('E-mail : jimmy.salies@gmail.com');
+    $('#6').append('Hobbies : aeromodelisme, jeux video, programmation');
 };
 
+function second() {
+    $('#info').empty();
+    $('#1').empty();
+    $('#2').empty();
+    $('#3').empty();
+    $('#4').empty();
+    $('#5').empty();
+    $('#6').empty();
+    $('#1').append('Age : 21ans');
+    $('#2').append('Type de jeux : FPS, Mmoba');
+    $('#3').append('Jeux du moment: lol, Civ6');
+    $('#4').append('Pseudo(par jeux) : Bakanos(lol), bob(minecraft)');
+    $('#5').append('E-mail : hamza.chegraoui@gmail.com');
+    $('#6').append('Hobbies : jeux video, mathematiques');
+};
 
-element1.addEventListener('click', crier());
+element1.addEventListener('mouseover',premier());
+element2.addEventListener('mouseover',second());
